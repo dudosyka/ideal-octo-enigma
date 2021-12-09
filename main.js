5eb47ceb0840917dc8e9d34d91476bc44381ae0f
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-12-10 01:28:33 +0300

    Faded title

--- main.js
+++ main.js
@@ -5,22 +5,42 @@ const dotsStyler = function () {
         document.getElementById("dot1").classList.remove("dot-active");
         document.getElementById("dot2").classList.add("dot-active");
         document.getElementById("dot3").classList.remove("dot-active");
+
+        $("#logo1").fadeOut(300);
+        $("#logo3").fadeOut(300);
+        setTimeout(() => {
+            $("#logo2").fadeIn(300);
+        }, 300)
     }
     if (hsuma === -200) {
         console.log(3);
         document.getElementById("dot1").classList.remove("dot-active");
         document.getElementById("dot2").classList.remove("dot-active");
         document.getElementById("dot3").classList.add("dot-active");
+
+        $("#logo1").fadeOut(300);
+        $("#logo2").fadeOut(300);
+        setTimeout(() => {
+            $("#logo3").fadeIn(300);
+        }, 300);
     }
     if (hsuma === -300 || hsuma === 0) {
         console.log(1);
         document.getElementById("dot1").classList.add("dot-active");
         document.getElementById("dot2").classList.remove("dot-active");
         document.getElementById("dot3").classList.remove("dot-active");
+
+        $("#logo2").fadeOut(300);
+        $("#logo3").fadeOut(300);
+        setTimeout(() => {
+            $("#logo1").fadeIn(300);
+        }, 300);
     }
 }
 
 $(document).ready(function(){
+    $("#logo2").fadeOut();
+    $("#logo3").fadeOut();
     $(".next").click(function(){
         hsuma += -100;
         dotsStyler();
